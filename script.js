9caa65bf87ab5da1609bd372168850df81a302ca
ricardo-cuesta <ricardo.cuesta@udea.edu.co>
2024-04-21 19:59:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,17 +13,39 @@ async function consultarApiC(url) {
   }
 }
 
+//console.log(value); // Devuelve us, por estar seleccionado por defecto
+
 async function obtenerDatosC(url) {  
+  var select = document.getElementById('comida');
+  var value = select.options[select.selectedIndex].value;
+
   const datos = await consultarApiC(url); 
 
   let meal_01=datos.meals[2].strInstructions 
-   console.log(datos.meals[2].strArea )
-  
+   //console.log(datos.meals[2].strArea )
+   //console.log(value);
+   console.log(value);
 
+if (value="recetas") {console.log("0");
+  
+} else {console.log("1");
+  
+}
+
+
+
+
+
+
+
+
+
+
+   
    let lee_menu = document.querySelector(".menu"); 
    let receta = document.querySelector(".menu");
    receta.innerHTML = meal_01;
-   console.log(meal_01)
+   //console.log(meal_01)
   
   const contError = document.querySelector(".error");
   const contWeather = document.querySelector(".weather");
