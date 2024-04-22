f0f4c12bd3e354ab302f38c58e64618965ed3410
ricardo-cuesta <ricardo.cuesta@udea.edu.co>
2024-04-21 19:54:43 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=d3c39f57206d5904890771c822ffaac3";
-//const apiKey = "&appid=d3c39f57206d5904890771c822ffaac3";
+"https://www.themealdb.com/api/json/v1/1/search.php?s="
 
-async function consultarApiClima(url) {
+async function consultarApiC(url) {
   try {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.error(`fallo la consulta a la api: ${error}`);
     const contError = document.querySelector(".error");
-    const contWeather = document.querySelector(".weather");
+    const contWeather = document.querySelector(".receta");
     contError.style.display = "block";
-    contWeather.style.display = "none";
   }
 }
 
-async function obtenerDatosClima(url) {
-  const datos = await consultarApiClima(url);
-  let nombreCiudad = document.querySelector(".city");
-  nombreCiudad.innerHTML = datos.name;
-  let temperatura = document.querySelector(".temp");
-  temperatura.innerHTML = datos.main.temp + "°C";
-  let humedad = document.querySelector(".humidity");
-  humedad.innerHTML = datos.main.humidity + "%";
-  let viento = document.querySelector(".wind");
-  viento.innerHTML = datos.wind.speed + "km/h";
-  let iconoClima = document.querySelector(".weather-icon");
-  const estadoClima = datos.weather[0].main;
-  iconoClima.src = "images/" + String(estadoClima).toLowerCase() + ".png";
+async function obtenerDatosC(url) {  
+  const datos = await consultarApiC(url); 
 
+  let meal_01=datos.meals[2].strInstructions 
+   console.log(datos.meals[2].strArea )
+  
+
+   let lee_menu = document.querySelector(".menu"); 
+   let receta = document.querySelector(".menu");
+   receta.innerHTML = meal_01;
+   console.log(meal_01)
+  
   const contError = document.querySelector(".error");
   const contWeather = document.querySelector(".weather");
-  contError.style.display = "none";
-  contWeather.style.display = "block";
 }
 
 const searchButton = document.querySelector(".search button");
 const searchInput = document.querySelector(".search input");
+console.log(`a la consulta a la api:`);
 
 searchButton.addEventListener("click", () => {
-  const nombreCiudad = searchInput.value;
-  console.log(nombreCiudad);
-  const url = `${apiUrl}&q=${nombreCiudad}`;
-  console.log(url);
-  obtenerDatosClima(url);
+  //const nombreC = searchInput.value;
+  
+  const url = `${apiUrl}`;
+  //console.log(url);
+  obtenerDatosC(url);
+  
 });
-
-//https://api.openweathermap.org/data/2.5/weather?units=metric&appid=d3c39f57206d5904890771c822ffaac3&q=bogota
-
-/*(async() =>{
-    const datos = await consultarApiClima(apiUrl);
-    console.log(datos);
-})();*/
